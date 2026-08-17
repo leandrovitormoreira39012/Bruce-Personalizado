9eca6c3cd3bbf29ed33aba6cfab765d26ee9c9b4
leandrovitormoreira39012 <leandrovitormoreira390126548@gmail.com>
2026-08-17 09:01:50 -0300

    Create input.cpp

---
+++ boards/ambiente-01/input.cpp
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+
+void setupInput() {
+    // Configuração inicial dos pinos do Joystick KY-023 ou touch se necessário
+}
+
+void InputHandler() {
+    // Tratamento de leitura dos inputs do ambiente
+}
+
