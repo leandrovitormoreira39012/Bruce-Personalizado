9466e23e592853ea07ea20b2819a68d5297c5407
leandrovitormoreira39012 <leandrovitormoreira390126548@gmail.com>
2026-08-18 01:38:20 -0300

    Update pins_arduino.h

--- boards/ambiente-03/pins_arduino.h
+++ boards/ambiente-03/pins_arduino.h
@@ -3,45 +3,46 @@
 #ifndef PINS_ARDUINO_AMBIENTE_03_H
 #define PINS_ARDUINO_AMBIENTE_03_H
 
-/* SPI */
+/* SPI Compartilhado (VSPI) */
 #define PIN_SPI_SCK     18
 #define PIN_SPI_MOSI    23
 #define PIN_SPI_MISO    19
 #define PIN_SPI_SS      5
 
-/* Display 1.8" */
+/* Display 1.8" 128x160 */
 #define PIN_TFT_CS      15
 #define PIN_TFT_DC      2
 #define PIN_TFT_RST     4
-#define PIN_TFT_BL      21
+#define PIN_TFT_BL      32
 #define TFT_WIDTH       128
 #define TFT_HEIGHT      160
 
-/* SD */
-#define PIN_SD_CS       4
+/* SD Card (SPI) */
+#define PIN_SD_CS       14
 
-/* NRF24 */
+/* NRF24L01+PA */
 #define PIN_NRF24_CE    25
+#define PIN_NRF24_CS    26
 
 /* CC1101 */
-#define PIN_CC1101_GDO0 26
-#define PIN_CC1101_CS   21
+#define PIN_CC1101_GDO0 27
+#define PIN_CC1101_CS   22
 
-/* IR */
-#define PIN_IR_TX       27
-#define PIN_IR_RX       13
+/* Infravermelho (IR) */
+#define PIN_IR_TX       33
+#define PIN_IR_RX       32
 
-/* GPS (UART) */
-#define PIN_GPS_TX      16
-#define PIN_GPS_RX      17
+/* GPS (UART2) */
+#define PIN_GPS_TX      17
+#define PIN_GPS_RX      16
 
 /* PN532 (I2C) */
 #define PIN_I2C_SDA     21
 #define PIN_I2C_SCL     22
 
-/* Joystick */
+/* Joystick KY-023 */
 #define PIN_JOY_VRX     34
 #define PIN_JOY_VRY     35
-#define PIN_JOY_SW      32
+#define PIN_JOY_SW      36
 
 #endif /* PINS_ARDUINO_AMBIENTE_03_H */
