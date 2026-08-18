20df101ba296fc618a61912b75219c41dbed76d2
leandrovitormoreira39012 <leandrovitormoreira390126548@gmail.com>
2026-08-18 13:43:01 -0300

    Update pins_arduino.h

--- boards/ambiente-02/pins_arduino.h
+++ boards/ambiente-02/pins_arduino.h
@@ -1,4 +1,4 @@
-/* ADDED: pins_arduino.h for Ambiente-02 (ST7789 240x240 + Joystick) */
+/* ADDED: pins_arduino.h for Ambiente-02 (ST7789 240x320 2.8" + Touch + SD) */
 
 #ifndef PINS_ARDUINO_AMBIENTE_02_H
 #define PINS_ARDUINO_AMBIENTE_02_H
@@ -9,15 +9,19 @@
 #define PIN_SPI_MISO    10
 #define PIN_SPI_SS      5
 
-/* Display ST7789 240x240 (7 Pinos) */
-#define PIN_TFT_CS      -1
+/* Display 2.8" ST7789 240x320 */
+#define PIN_TFT_CS      17
 #define PIN_TFT_DC      16
 #define PIN_TFT_RST     15
 #define PIN_TFT_BL      14
 #define TFT_WIDTH       240
-#define TFT_HEIGHT      240
+#define TFT_HEIGHT      320
 
-/* SD Card (SPI) */
+/* Touch Controller (XPT2046) */
+#define PIN_TOUCH_CS    41
+#define PIN_TOUCH_IRQ   -1
+
+/* SD Card (SPI embutido na tela) */
 #define PIN_SD_CS       4
 
 /* NRF24L01+PA */
@@ -40,7 +44,7 @@
 #define PIN_I2C_SDA     8
 #define PIN_I2C_SCL     9
 
-/* Joystick KY-023 (Botão no GPIO 6) */
+/* Joystick / Botões (Botão SW no GPIO 6) */
 #define PIN_JOY_VRX     1
 #define PIN_JOY_VRY     2
 #define PIN_JOY_SW      6
