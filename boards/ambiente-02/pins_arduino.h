62dbe4c36382eceae04379d422eb2e88858e48ca
leandrovitormoreira39012 <leandrovitormoreira390126548@gmail.com>
2026-08-18 10:52:49 -0300

    Update pins_arduino.h

--- boards/ambiente-02/pins_arduino.h
+++ boards/ambiente-02/pins_arduino.h
@@ -3,22 +3,22 @@
 #ifndef PINS_ARDUINO_AMBIENTE_02_H
 #define PINS_ARDUINO_AMBIENTE_02_H
 
-/* SPI Compartilhado (S3) */
-#define PIN_SPI_SCK     18
+/* SPI Compartilhado (S3) - Ajustado para Pinos Seguros */
+#define PIN_SPI_SCK     12
 #define PIN_SPI_MOSI    11
 #define PIN_SPI_MISO    10
-#define PIN_SPI_SS      4
+#define PIN_SPI_SS      5
 
 /* Display 2.8" ST7789 240x320 */
 #define PIN_TFT_CS      17
 #define PIN_TFT_DC      16
 #define PIN_TFT_RST     15
-#define PIN_TFT_BL      6
+#define PIN_TFT_BL      14
 #define TFT_WIDTH       240
 #define TFT_HEIGHT      320
 
 /* Touch Controller */
-#define PIN_TOUCH_CS    5
+#define PIN_TOUCH_CS    33
 #define PIN_TOUCH_IRQ   -1
 
 /* SD Card (SPI) */
