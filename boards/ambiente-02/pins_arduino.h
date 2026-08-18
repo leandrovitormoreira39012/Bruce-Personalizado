8183af672434c0632c8c1aef02817e10fae5348b
leandrovitormoreira39012 <leandrovitormoreira390126548@gmail.com>
2026-08-18 12:33:19 -0300

    Update pins_arduino.h

--- boards/ambiente-02/pins_arduino.h
+++ boards/ambiente-02/pins_arduino.h
@@ -1,30 +1,26 @@
-/* ADDED: pins_arduino.h for Ambiente-02 (ST7789 240x320 + touch) */
+/* ADDED: pins_arduino.h for Ambiente-02 (ST7789 240x240 + Joystick) */
 
 #ifndef PINS_ARDUINO_AMBIENTE_02_H
 #define PINS_ARDUINO_AMBIENTE_02_H
 
-/* SPI Compartilhado (S3) - Atualizado para Pinos Seguros */
+/* SPI Compartilhado (S3) - Pinos Seguros */
 #define PIN_SPI_SCK     12
 #define PIN_SPI_MOSI    11
 #define PIN_SPI_MISO    10
 #define PIN_SPI_SS      5
 
-/* Display 2.8" ST7789 240x320 */
-#define PIN_TFT_CS      17
+/* Display ST7789 240x240 (7 Pinos) */
+#define PIN_TFT_CS      -1
 #define PIN_TFT_DC      16
 #define PIN_TFT_RST     15
 #define PIN_TFT_BL      14
 #define TFT_WIDTH       240
-#define TFT_HEIGHT      320
-
-/* Touch Controller (Movido para 41) */
-#define PIN_TOUCH_CS    41
-#define PIN_TOUCH_IRQ   -1
+#define TFT_HEIGHT      240
 
 /* SD Card (SPI) */
 #define PIN_SD_CS       4
 
-/* NRF24L01+PA (CS movido para 42) */
+/* NRF24L01+PA */
 #define PIN_NRF24_CE    13
 #define PIN_NRF24_CS    42
 
@@ -44,9 +40,9 @@
 #define PIN_I2C_SDA     8
 #define PIN_I2C_SCL     9
 
-/* Joystick KY-023 */
+/* Joystick KY-023 (Botão no GPIO 6) */
 #define PIN_JOY_VRX     1
 #define PIN_JOY_VRY     2
-#define PIN_JOY_SW      3
+#define PIN_JOY_SW      6
 
 #endif /* PINS_ARDUINO_AMBIENTE_02_H */
