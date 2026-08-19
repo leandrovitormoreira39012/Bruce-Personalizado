a5e18a1cb1fb853f374da281431cf7bb057c86be
leandrovitormoreira39012 <leandrovitormoreira390126548@gmail.com>
2026-08-19 09:26:37 -0300

    Update pins_arduino.h

--- boards/Marauder-Radio/pins_arduino.h
+++ boards/Marauder-Radio/pins_arduino.h
@@ -1,7 +1,7 @@
 #ifndef PINS_ARDUINO_MARAUDER_RADIO_H
 #define PINS_ARDUINO_MARAUDER_RADIO_H
 
-/* SPI Principal (Display, SD e Touch) */
+/* SPI Principal */
 #define PIN_SPI_SCK     18
 #define PIN_SPI_MOSI    23
 #define PIN_SPI_MISO    19
@@ -15,36 +15,4 @@
 #define TFT_WIDTH       240
 #define TFT_HEIGHT      320
 
-/* Touch Controller (Movido para o pino 21 para liberar o 33) */
-#define PIN_TOUCH_CS    21
-
-/* Cartão SD */
-#define PIN_SD_CS       4
-
-/* NRF24L01 (2.4GHz) */
-#define PIN_NRF24_CE    25
-#define PIN_NRF24_CS    PIN_SPI_SS
-
-/* CC1101 (433MHz) */
-#define PIN_CC1101_GDO0 26
-#define PIN_CC1101_CS   22
-
-/* Infravermelho (IR) */
-#define PIN_IR_TX       27
-#define PIN_IR_RX       33
-
-/* GPS (Neo 6M) */
-#define PIN_GPS_TX      39
-#define PIN_GPS_RX      40
-
-/* PN532 NFC (I2C) */
-#define PIN_I2C_SDA     4
-#define PIN_I2C_SCL     5
-
-/* Joystick / Navegação */
-#define PIN_JOY_VRX     36
-#define PIN_JOY_VRY     37
-#define PIN_JOY_SW      6
-
-#endif
-
+#endif /* PINS_ARDUINO_MARAUDER_RADIO_H */
