951354b58946624065608bcb7a1e9c7b52fd1063
leandrovitormoreira39012 <leandrovitormoreira390126548@gmail.com>
2026-09-19 20:08:01 -0300

    Update pins_arduino.h
    Módulos
    Nrf24l01+pna
    Cc1101
    Gps neo 6m v2
    Pn532
    Ir transmiter 
    Ir receiver
    Leitor sd
    Tela 2.8 240x320 com touch
    Esp32 devkit 30 pinos

--- boards/marauder-touch/pins_arduino.h
+++ boards/marauder-touch/pins_arduino.h
@@ -7,58 +7,20 @@
 #define USB_VID 0x303a
 #define USB_PID 0x1001
 
-static const uint8_t TX = 43;
-static const uint8_t RX = 44;
+// --- Definições Padrão I2C e SPI ---
+#define SDA 33
+#define SCL 26
+#define SCK 18
+#define MISO 19
+#define MOSI 23
+#define SS 17
 
-static const uint8_t TXD2 = 1;
-static const uint8_t RXD2 = 2;
-
-static const uint8_t SDA = 13;
-static const uint8_t SCL = 15;
-
-// Modified elsewhere
-static const uint8_t SS = -1;
-static const uint8_t MOSI = -1;
-static const uint8_t MISO = -1;
-static const uint8_t SCK = -1;
-
-static const uint8_t G0 = 0;
-static const uint8_t G1 = 1;
-static const uint8_t G2 = 2;
-static const uint8_t G3 = 3;
-static const uint8_t G4 = 4;
-static const uint8_t G5 = 5;
-static const uint8_t G6 = 6;
-static const uint8_t G7 = 7;
-static const uint8_t G8 = 8;
-static const uint8_t G9 = 9;
-static const uint8_t G10 = 10;
-static const uint8_t G11 = 11;
-static const uint8_t G12 = 12;
-static const uint8_t G13 = 13;
-static const uint8_t G14 = 14;
-static const uint8_t G15 = 15;
-static const uint8_t G39 = 39;
-static const uint8_t G40 = 40;
-static const uint8_t G41 = 41;
-static const uint8_t G42 = 42;
-static const uint8_t G43 = 43;
-static const uint8_t G44 = 44;
-static const uint8_t G46 = 46;
-
-static const uint8_t ADC1 = 7;
-static const uint8_t ADC2 = 8;
-
-#define RGB_LED 21
-
-#define BAD_TX 4
-#define BAD_RX 13
-
-// SERIAL (GPS) dedicated pins
+// --- Módulo GPS NEO-6M ---
 #define SERIAL_TX 4
-#define SERIAL_RX 13
+#define SERIAL_RX 22
 #define GPS_SERIAL_TX SERIAL_TX
 #define GPS_SERIAL_RX SERIAL_RX
+
 #define HAS_BTN 0
 #define BTN_ALIAS "\"Ok\""
 #define BTN_PIN 0
@@ -68,25 +30,44 @@ static const uint8_t ADC2 = 8;
 #define LED_ON HIGH
 #define LED_OFF LOW
 
-#define CC1101_GDO0_PIN -1
-#define CC1101_SS_PIN -1
+// --- Barramento SPI Principal (Compartilhado) ---
+#define SPI_SCK_PIN 18
+#define SPI_MISO_PIN 19
+#define SPI_MOSI_PIN 23
+#define SPI_SS_PIN 17
+
+// --- Módulo CC1101 (Sub-GHz) ---
+#define CC1101_GDO0_PIN 2
+#define CC1101_SS_PIN 15
 #define CC1101_MOSI_PIN SPI_MOSI_PIN
 #define CC1101_SCK_PIN SPI_SCK_PIN
 #define CC1101_MISO_PIN SPI_MISO_PIN
 
-#define NRF24_CE_PIN -1
-#define NRF24_SS_PIN -1
+// --- Módulo NRF24L01+PA+LNA (2.4GHz) ---
+#define NRF24_CE_PIN 27
+#define NRF24_SS_PIN 14
 #define NRF24_MOSI_PIN SPI_MOSI_PIN
 #define NRF24_SCK_PIN SPI_SCK_PIN
 #define NRF24_MISO_PIN SPI_MISO_PIN
+
+// --- Módulos IR (Infravermelho) ---
+#define HAS_IR 1
+#define IR_TX_PIN 25
+#define IR_RX_PIN 34
+
+// --- Módulo PN532 (NFC / RFID via I2C) ---
+#define HAS_PN532 1
+#define PN532_SDA 33
+#define PN532_SCL 26
 
 #define FP 1
 #define FM 2
 #define FG 3
 
+// --- Configurações da Tela ILI9341 2.8" Touch ---
 #define HAS_SCREEN 1
 #define ROTATION 3
-#define MINBRIGHT 160
+#define TFT_BRIGHT 160
 
 #define USER_SETUP_LOADED 1
 #define ILI9341_DRIVER 1
@@ -101,21 +82,19 @@ static const uint8_t ADC2 = 8;
 #define SMOOTH_FONT 1
 #define SPI_FREQUENCY 40000000
 #define SPI_READ_FREQUENCY 16000000
-#define SPI_TOUCH_FREQUENCY 2500000
+#define SPI_TOUCH_FREQUENCY 25000000
 #define TOUCH_CS 21
 #define HAS_TOUCH 1
 
-// #define SDCARD_CS 12
+// --- Cartão Micro SD ---
+#define SDCARD_CS 12
 #define SDCARD_SCK 18
 #define SDCARD_MISO 19
 #define SDCARD_MOSI 23
 
-#define GROVE_SDA 33
-#define GROVE_SCL 22
-
-#define SPI_SCK_PIN 18
-#define SPI_MISO_PIN 19
-#define SPI_MOSI_PIN 23
-#define SPI_SS_PIN 1
+// --- Módulo BadUSB ---
+#define BAD_RX -1
+#define BAD_TX -1
 
 #endif /* Pins_Arduino_h */
+
